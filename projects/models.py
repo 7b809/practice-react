30bfe36aa11689adbed254a1fdafd48e39b1ba74
7b809 <ej818793@gmail.com>
2025-09-02 23:05:06 +0530

    created a sample model for projects app in projects/models.py folder

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import  uuid
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    
+    # description can be empty null is for database and blank is for django i.e when usign in form.
+    description = models.TextField(null=True,blank=True)
+
+    demo_link = models.CharField(max=2000,null=True,blank=True)
+    source_link = models.CharField(max=2000,null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
