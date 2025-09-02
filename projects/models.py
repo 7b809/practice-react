357e64bc487ab8d8bfc5811b1e99006de9fc784e
7b809 <ej818793@gmail.com>
2025-09-02 23:07:48 +0530

    a bug appered in prevoid code - solved.

--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     # description can be empty null is for database and blank is for django i.e when usign in form.
     description = models.TextField(null=True,blank=True)
 
-    demo_link = models.CharField(max=2000,null=True,blank=True)
-    source_link = models.CharField(max=2000,null=True,blank=True)
+    demo_link = models.CharField(max_length=2000,null=True,blank=True)
+    source_link = models.CharField(max_length=2000,null=True,blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
